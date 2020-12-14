a8eabb904557d1cc83ed008faee96054b497b8b2
ykst <ykst615@qq.com>
2020-12-15 00:19:41 +0800

    增强开发体验 0.0.1

---
+++ webpack/scripts/constant.js
@@ -0,0 +1,3 @@
+module.exports = {
+  SVRCODECOMPLETED: 'SVRCODECOMPLETED', //表示服务端代码编译完成的标志
+};
---
+++ webpack/svr-code-server.js
@@ -0,0 +1,11 @@
+//公用配置文件，定义一些通用的数据
+const proConfig = require('../../src/share/pro-config');
+
+//node server port
+const nodeServerPort = proConfig.nodeServerPort;
+
+//启动前检查端口是否占用，杀掉占用端口的进程
+require('./free-port')(nodeServerPort);
+
+//引入打包后的入口文件,这个入口我们在 webpack 配置中已设置好
+require('../../dist/server/app');
