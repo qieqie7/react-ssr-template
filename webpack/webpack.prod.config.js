e7abcf76169172f0add0636f885544639fd2c0ae
ykst <ykst615@qq.com>
2020-12-22 23:07:26 +0800

    完整 client 端生产环境配置

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,18 +1,35 @@
 const path = require('path');
 const webpack = require('webpack');
+// 提取css
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// 压缩和优化css
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { WebpackManifestPlugin: ManifestPlugin } = require('webpack-manifest-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+process.env.BABEL_ENV = 'development'; //指定 babel 编译环境
 
 //定一个通用的路径转换方法
 const resolvePath = pathStr => path.resolve(__dirname, pathStr);
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     main: resolvePath('../src/client/app/index.js'), //入口文件
   },
+  /**
+   * NOTE:
+   * 太坑了太坑了
+   * 如果不指定编译成 es5，webpack5 打包的时候用 箭头函数 去组织代码
+   * uglifyjs 无法处理 es6 的语法
+   * 就一直保存...
+   * */
+  target: ['web', 'es5'],
   output: {
-    filename: '[name].js', //设置打包后的文件名
+    filename: 'js/[name].[chunkhash:8].js', //设置打包后的文件名
     path: resolvePath('../dist/static'), //设置构建结果的输出目录
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -45,7 +62,40 @@ module.exports = {
       __IS_PROD: true,
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
+      filename: 'css/[name].[contenthash:8].css',
     }),
+    new ManifestPlugin({
+      fileName: '../server/asset-manifest.json',
+    }),
+    new CleanWebpackPlugin(),
   ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: true,
+            drop_debugger: true,
+          },
+          warnings: false,
+          ie8: true,
+          output: {
+            comments: false,
+          },
+        },
+        cache: true,
+        sourceMap: false,
+      }),
+      new OptimizeCSSAssetsPlugin(),
+    ],
+    splitChunks: {
+      cacheGroups: {
+        libs: {
+          test: /node_modules/,
+          chunks: 'initial',
+          name: 'libs',
+        },
+      },
+    },
+  },
 };
